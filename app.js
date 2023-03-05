754a3c4533bfe5ad62674018318ca9e83ce544ae
chetan gupta <chetangupta123raj@gmail.com>
2023-03-05 10:29:35 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-const express = require('express');
-
-const app = express();
-console.log("server");
-const port = 5000;
-
-const start = async () => {
-  try {
-          app.listen(port, () => {
-                  console.log(`server is listening on port ${port}...`)
-          });
-  } catch (error) {
-          console.log(error);
-  }
-};
-start();
+console.log("hello world")
